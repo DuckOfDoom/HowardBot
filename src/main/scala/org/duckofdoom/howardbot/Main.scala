947750fd0fbfbb93f870f9f7079f61001053ed62
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-07-15 00:02:27 +0300

    Playing around

--- src/main/scala/org/duckofdoom/howardbot/Main.scala
+++ src/main/scala/org/duckofdoom/howardbot/Main.scala
@@ -8,12 +8,16 @@ import slogging._
 
 object Main extends StrictLogging {
   def main(args: Array[String]): Unit = {
-
     LoggerConfig.factory = PrintLoggerFactory()
-    LoggerConfig.level = LogLevel.DEBUG
+    LoggerConfig.level = LogLevel.TRACE
 
-    logger.error("s")
-    
+    henloWorld()
+
+  }
+
+  private def runBot(): Unit = {
+    logger.info("Running bot.")
+
     // To run spawn the bot
     val bot = new RandomBot("249399445:AAHX9cG3MCsF9kJ9Y-c-LmAF3_RCALrIG2s")
     val eol = bot.run()
@@ -22,5 +26,27 @@ object Main extends StrictLogging {
     bot.shutdown() // initiate shutdown
     // Wait for the bot end-of-life
     Await.result(eol, Duration.Inf)
+  }
+
+  private def henloWorld(): Unit = {
+
+    val l = List(1, "ad", true)
+
+    println(l)
+    println(l.productArity)
+    
+    val d1 = new derp1(1, "1")
+    val d2 = new derp2(2, "2")
+    val d22 = new derp2(3, "2")
+    
+    println(d2.equals(d22))
+  }
+
+  class derp1(x: Int, s: String) {
+
+  }
+  
+  case class derp2(x: Int, s: String) {
+
   }
 }
