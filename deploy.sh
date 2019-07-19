7b0a7f827ee83c1463ea3143ae839f12f2390f1c
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-07-19 09:00:55 +0300

    Added deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-echo 'hey'
+JAR_PATH='target/scala-2.12/HowardBot-assembly-0.1.jar'
+CONFIG_PATH='config.json'
+
+scp $JAR_PATH root@142.93.170.84:~/bot.jar
+scp $CONFIG_PATH root@142.93.170.84:~/CONFIG_PATH
