67141f7b23454911e191ac1414a96b53065c5984
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-07-24 15:40:16 +0300

    Updated deploy to run sbt

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@ JAR_PATH='target/scala-2.12/HowardBot-assembly-0.1.jar'
 CONFIG_PATH='config.json'
 ADDRESS='root@142.93.170.84'
 
+echo Starting sbt assembly...
+sbt assembly
+
+echo Assembly finished, pushing artifacts...
+
 scp $JAR_PATH $ADDRESS:~/bot.jar
 #scp $CONFIG_PATH $ADDRESS:~/$CONFIG_PATH
 ssh $ADDRESS
