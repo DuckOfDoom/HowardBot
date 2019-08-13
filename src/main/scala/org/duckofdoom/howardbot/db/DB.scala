86c3c5fe37ab486f79ef1b442699d1c139069fec
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-08-13 19:21:16 +0300

    Fixed UserState parsing

--- src/main/scala/org/duckofdoom/howardbot/db/DB.scala
+++ src/main/scala/org/duckofdoom/howardbot/db/DB.scala
@@ -7,6 +7,7 @@ import io.circe.syntax._
 import doobie._
 import doobie.implicits._
 import doobie.util.log.{ExecFailure, ProcessingFailure}
+import io.circe.{Decoder, Encoder}
 import org.duckofdoom.howardbot._
 import org.duckofdoom.howardbot.db.dto._
 import slogging.StrictLogging
@@ -17,30 +18,37 @@ trait DB {
   def users: List[User]
   def getUser(userId: Long): Option[User]
   def getUserByTelegramId(userId: Long): Option[User]
-  def putUser(userId: Long, firstName:String, lastName: Option[String], username: Option[String]): Option[User]
-  def updateUser(user:User): Unit
+  def putUser(userId: Long,
+              firstName: String,
+              lastName: Option[String],
+              username: Option[String]): Option[User]
+  def updateUser(user: User): Unit
 }
 
-class DoobieDB(config: PostgresConfig) extends DB 
-  with StrictLogging { 
+class DoobieDB(config: PostgresConfig) extends DB with StrictLogging {
 
   implicit val executionContext: ExecutionContextExecutor = ExecutionContext.global
-  implicit val contextShift: ContextShift[IO] = IO.contextShift(executionContext)
-  
+  implicit val contextShift: ContextShift[IO]             = IO.contextShift(executionContext)
+
   implicit val getUserState: Get[UserState] = Get[String].tmap(str => {
+
+    implicit val decoder: Decoder[UserState] = UserState.decoder
+
     decode[UserState](str) match {
-      case Left(err) => 
+      case Left(err) =>
         logger.error(s"Failed to deserialize user state '$str'\n$err.\nReturning default state.")
         UserState()
       case Right(st) => st
     }
   })
-  
+
   implicit val putUserState: Put[UserState] = Put[String].tcontramap(_.asJson.toString)
 
-  implicit val logHandler : LogHandler = LogHandler {
-    case ExecFailure(str, args, _, ex) => logger.error(s"ExecFailure: $str\nArgs: $args\nException: $ex")
-    case ProcessingFailure(str, args, _, _, ex) => logger.error(s"ProcessingFailure: $str \nArgs: $args\n Exception: $ex")
+  implicit val logHandler: LogHandler = LogHandler {
+    case ExecFailure(str, args, _, ex) =>
+      logger.error(s"ExecFailure: $str\nArgs: $args\nException: $ex")
+    case ProcessingFailure(str, args, _, _, ex) =>
+      logger.error(s"ProcessingFailure: $str \nArgs: $args\n Exception: $ex")
 //    case Success(str, args, t1, t2) => logger.info(s"$str \nArgs: $args, $t1, $t2")
     case _ => Unit
   }
@@ -54,7 +62,7 @@ class DoobieDB(config: PostgresConfig) extends DB
   )
 
   initTable()
-  
+
   private def initTable(): Unit = {
     sql"""
     CREATE TABLE IF NOT EXISTS users (  
@@ -63,10 +71,8 @@ class DoobieDB(config: PostgresConfig) extends DB
       username VARCHAR,
       firstName VARCHAR,
       lastName VARCHAR,
-      state VARCHAR DEFAULT '' 
-    )"""
-      .update
-      .run
+      state VARCHAR DEFAULT '{}' 
+    )""".update.run
       .transact(transactor)
       .unsafeRunSync
   }
@@ -77,11 +83,11 @@ class DoobieDB(config: PostgresConfig) extends DB
       .to[List]
       .transact(transactor)
       .unsafeRunSync()
-    
+
     logger.info(s"Selected ${users.length} users")
     users
   }
-  
+
   override def getUserByTelegramId(userId: Long): Option[User] = {
     sql"select * from users where userId=$userId"
       .query[User]
@@ -96,22 +102,27 @@ class DoobieDB(config: PostgresConfig) extends DB
       .unsafeRunSync()
   }
 
-  def putUser(userId: Long, firstName:String, lastName: Option[String], username: Option[String]): Option[User] = {
+  def putUser(userId: Long,
+              firstName: String,
+              lastName: Option[String],
+              username: Option[String]): Option[User] = {
     logger.info(s"Creating new user: $userId / $username / $firstName / $lastName")
-    
+
     val conn = for {
-      _  <- sql"""
+      _ <- sql"""
             insert into users (userId, username, firstName, lastName) 
             values ($userId, $username, $firstName, $lastName)""".update.run
 
       id <- sql"select lastval()".query[Long].unique
       p  <- selectUserQuery(id)
     } yield p
-    
+
     conn.transact(transactor).unsafeRunSync()
   }
-  
+
   def updateUser(user: User): Unit = {
+    implicit val stateEncoder: Encoder[UserState] = UserState.encoder
+
     sql"""UPDATE users  
     SET 
       userId = ${user.userId},
@@ -119,14 +130,12 @@ class DoobieDB(config: PostgresConfig) extends DB
       lastName = ${user.lastName},
       username = ${user.username},
       state = ${user.state.asJson.toString}      
-    WHERE id = ${user.id}"""
-      .update
-      .run
+    WHERE id = ${user.id}""".update.run
       .transact(transactor)
       .unsafeRunSync()
   }
-  
-  private def selectUserQuery(id:Long) : ConnectionIO[Option[User]] = {
+
+  private def selectUserQuery(id: Long): ConnectionIO[Option[User]] = {
     sql"select * from users where id=$id"
       .query[User]
       .option
--- src/main/scala/org/duckofdoom/howardbot/db/dto/User.scala
+++ src/main/scala/org/duckofdoom/howardbot/db/dto/User.scala
@@ -1,21 +1,27 @@
 package org.duckofdoom.howardbot.db.dto
 
+import cats.syntax.either._
 import io.circe.{Decoder, Encoder, HCursor, Json}
 
-case class User(id: Long, userId: Int, firstName:String, lastName: String, username:String, state: UserState)
+case class UserState(var menuPage: Int = 1)
 
-case class UserState(var menuPage: Int = 1) {
-  
-  implicit val encoder: Encoder[UserState] = (a: UserState) => Json.obj(
-    ("menuPage", Json.fromInt(a.menuPage))
+object UserState {
+
+  implicit val encoder: Encoder[UserState] = (a: UserState) =>
+    Json.obj(
+      ("menuPage", Json.fromInt(a.menuPage))
   )
 
-  implicit val decodeFoo: Decoder[UserState] = (c: HCursor) => {
-    for {
-      foo <- c.downField("menuPage").as[Int]
-    } yield {
-      UserState(foo)
-    }
+  // This decoder should not fail ever!
+  implicit val decoder: Decoder[UserState] = (c: HCursor) => {
+    val menuPage: Int = c.downField("menuPage").as[Int].getOrElse(1)
+    UserState(menuPage).asRight
   }
 }
 
+case class User(id: Long,
+                userId: Int,
+                firstName: String,
+                lastName: String,
+                username: String,
+                state: UserState)
